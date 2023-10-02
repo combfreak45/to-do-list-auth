867514b4b18dfe16d958e68417cb280271848755
combfreak45 <113178794+combfreak45@users.noreply.github.com>
2023-10-02 16:38:23 +0530

    3rd day work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import ToDo from "./Components/ToDo";
 import {GrAddCircle} from 'react-icons/gr'
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 const App = () => {
     const [data, setData] = useState([]);
+    const [input,setInput] = useState('')
     useEffect(()=>{
         const q = query(collection(db,'todos'))
         const d = onSnapshot(q,(querySnap)=>{
@@ -17,25 +18,51 @@ const App = () => {
         })
         return () => d()
     },[])
+
+    const toggleComponent = async (value) => {
+                await updateDoc(doc(db,'todos',value.id),{
+                  completed: !value.completed
+                })
+    }
+
+   const createTodo = async (e) =>{
+        e.preventDefault(e)
+        if(input===''){
+          alert('enter the value')
+          return
+        }
+        await addDoc(collection(db,'todos'),{
+          text: input,
+          completed: false,
+        })
+        setInput('')
+   }
+
+   const deleteTodo = async(id)=>{
+    await deleteDoc(doc(db,'todos',id))
+   }
+ 
  return (
    <div className="bg-black min-h-screen min-w-screen py-[10rem] pl-[25rem] ">
      <div className="bg-white w-[30rem] min-h-[20rem] rounded-3xl p-11 flex flex-col gap-9">
        <div className=" h-[3rem]  text-center text-2xl p-2 font-extrabold">
          TO DO LIST
        </div>
-       <div className="bg-black h-[4em] rounded-3xl text-white text-center text-xl p-6 flex gap-10">
+       <form className="bg-black h-[4em] rounded-3xl text-white text-center text-xl p-6 flex gap-10" onSubmit={createTodo}>
          <input
            type="text"
            placeholder="ADD TASK"
            className="h-[2rem] rounded-lg text-black p-3"
+           value={input}
+           onChange={(e)=>setInput(e.target.value)}
          />
          <button>
            <GrAddCircle className="w-[2rem] h-[2rem] bg-white rounded-2xl" />
          </button>
-       </div>
+       </form>
        <div className="bg-black min-h-[10rem] rounded-3xl p-5 flex flex-col gap-5">
-         {data.map((value,index) => (
-           <ToDo key={index} value={value} />
+         {data.map((value,index,) => (
+           <ToDo key={index} value={value} toggleComponent={toggleComponent} deleteTodo={deleteTodo}/>
          ))}
        </div>
      </div>
--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import {BsTrash} from 'react-icons/bs'
-const ToDo = ({value}) => {
+const ToDo = ({value,toggleComponent,deleteTodo}) => {
   return (
-    <div className='bg-white flex  gap-10 h-[3rem] rounded-2xl p-2 justify-between'>
-    <input type="checkbox" />
-    <div className='bg-black text-white text-center rounded-lg w-[20rem]'>{value.text}</div>
-    <BsTrash size={30}/>
+    <div className="bg-white flex  gap-10 h-[3rem] rounded-2xl p-2 justify-between">
+      <input
+        type="checkbox"
+        checked={`${value.completed ? "check" : ""}`}
+        onChange={() => toggleComponent(value)}
+      />
+      <div
+        className={`bg-black text-white text-center rounded-lg w-[20rem] ${
+          value.completed ? "line-through" : "no-underline"
+        } hover:cursor-pointer`}
+        onClick={() => toggleComponent(value)}
+      >
+        {value.text}
+      </div>
+      <button onClick={()=>deleteTodo(value.id)}>
+        <BsTrash size={30} />
+      </button>
     </div>
-  )
+  );
 }
 
 export default ToDo
